cac45549f573ffdf0fe1e3004b97abc85a1bfebb
vswapna3202 <vswapna3202@gmail.com>
2023-11-05 19:44:55 +0000

    Feat: Added test for RootController

---
+++ src/test/java/com/thealisters/musicquizapp/server/controller/RootControllerTest.java
@@ -0,0 +1,27 @@
+package com.thealisters.musicquizapp.server.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RootControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void handleRoot() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("The AListers Music Quiz API!"));
+    }
+
+}
