a2c75d4945b51dd871c009724672a51c84da8cb0
vswapna3202 <vswapna3202@gmail.com>
2023-11-06 12:41:41 +0000

    Feat: Added code to find user high scores and positions

--- src/main/java/com/thealisters/musicquizapp/server/service/HighScoreService.java
+++ src/main/java/com/thealisters/musicquizapp/server/service/HighScoreService.java
@@ -2,7 +2,11 @@ package com.thealisters.musicquizapp.server.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface HighScoreService {
+
+    List<Object[]> getTopUserScoresWithPosition(String userId, Integer limit);
 
 }
--- src/main/java/com/thealisters/musicquizapp/server/service/HighScoreServiceImpl.java
+++ src/main/java/com/thealisters/musicquizapp/server/service/HighScoreServiceImpl.java
@@ -1,7 +1,17 @@
 package com.thealisters.musicquizapp.server.service;
 
+import com.thealisters.musicquizapp.server.repository.GameScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HighScoreServiceImpl implements HighScoreService {
+
+    @Autowired
+    GameScoreRepository gameScoreRepository;
+    public List<Object[]> getTopUserScoresWithPosition(String userId, Integer limit){
+        return gameScoreRepository.getTopUserScoresWithPosition(userId, limit);
+    }
 }
